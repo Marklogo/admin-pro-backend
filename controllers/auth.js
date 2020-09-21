a24826a9c0c895424ea21a98ce6fc393bd26a7cf
MarkLoGo <marklogo@gmail.com>
2020-09-21 23:43:19 +0200

    Seccion 12 -- Google

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,40 +1,36 @@
 const { response } = require("express");
-const bcrypt = require('bcryptjs')
-const usuario = require("../models/usuario");
-const {generarJWT} = require("../helpers/jwt")
-
-
+const bcrypt = require("bcryptjs");
+const Usuario = require("../models/usuario");
+const { generarJWT } = require("../helpers/jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async (req, res = response) => {
-    const {email, password } = req.body;
-    try {
+  const { email, password } = req.body;
+  try {
+    const usuarioDB = await Usuario.findOne({ email });
 
-        const usuarioDB = await usuario.findOne({email});
-        
-        if (!usuarioDB){
-            return res.status(404).json({
-                ok:false,
-                msg:"email no encontrado"
-            });
-        }
+    if (!usuarioDB) {
+      return res.status(404).json({
+        ok: false,
+        msg: "email no encontrado",
+      });
+    }
 
-        const validPassword = bcrypt.compareSync(password, usuarioDB.password);
-        if (!validPassword){
-            return res.status(400).json({
-                ok:false,
-                msg:"contraseña no valida"
-            });
-        }
+    const validPassword = bcrypt.compareSync(password, usuarioDB.password);
+    if (!validPassword) {
+      return res.status(400).json({
+        ok: false,
+        msg: "contraseña no valida",
+      });
+    }
 
     // Generar token
-        const token =  await generarJWT(usuarioDB.id);
-        
+    const token = await generarJWT(usuarioDB.id);
 
     res.json({
-        ok:true,
-        token
-    })
-
+      ok: true,
+      token,
+    });
   } catch (error) {
     console.log(error);
     res.status(500).json({
@@ -44,6 +40,45 @@ const login = async (req, res = response) => {
   }
 };
 
+const googleSignIn = async (req, res = response) => {
+  const googleToken = req.body.token;
+
+  try {
+    const { name, email, picture } = await googleVerify(googleToken);
+    const usuarioDB = await Usuario.findOne({ email });
+
+    let usuario;
+
+    if (!usuarioDB) {
+      usuario = new Usuario({
+        nombre: name,
+        email: email,
+        password: "@@@@",
+        img: picture,
+        google: true,
+      });
+    } else {
+      usuario = usuarioDB;
+      usuario.google = true;
+    }
+
+    await usuario.save();
+    // Generar token
+    const token = await generarJWT(usuario.id);
+
+    res.json({
+      ok: true,
+      token,
+    });
+  } catch (error) {
+    res.status(401).json({
+      ok: false,
+      msg: "Token no es correcto",
+    });
+  }
+};
+
 module.exports = {
-    login,
-}
+  login,
+  googleSignIn,
+};
--- index.js
+++ index.js
@@ -13,6 +13,9 @@ app.use(express.json());
 
 dbConection();
 
+//Directorio Publico
+app.use(express.static('public'));
+
 //rutas
 app.use ('/api/usuarios', require('./routes/usuarios'));
 app.use ('/api/hospitales', require('./routes/hospitales'));
@@ -20,7 +23,6 @@ app.use ('/api/medicos', require('./routes/medicos'));
 app.use ('/api/todo', require('./routes/busquedas'));
 app.use ('/api/upload', require('./routes/uploads'));
 app.use ('/api/login', require('./routes/auth'));
-
 
 
 app.listen(process.env.PORT,()=>{
